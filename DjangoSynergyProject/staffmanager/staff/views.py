a9fc6fb59d88171d8e63bd0771da38dfa374c543
Evgeny K. <evgeny.kruchinin@gmail.com>
2025-09-03 23:00:10 +0300

    Add files via upload

---
+++ DjangoSynergyProject/staffmanager/staff/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+"""Для простоты вместо html шаблона используем класс HttpResponse - из пакета позволяет отправить текстовое содержимое.
+Функция-обработчик index() принимает запрос request и возвращает ответ 'Здесь будет главная страница'  в теге h1 - заголовок первого уровня.
+Для вызова этой функции-обработчика надо в Джанго-проекте staffmanager в urls.py внести в список адресов urlpatterns
+адрес path('',views.index), по которому она находится. 
+Путь 'staffmanager.urls' к файлу с адресами urls.py содержится в переменной ROOT_URLCONF в настройках проекта staffmanager в файле settings.py """
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>Здесь будет главная страница<h1>")
